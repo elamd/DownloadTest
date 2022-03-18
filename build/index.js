882a639653dbd595507c4e4d6774891c020c2bda
Elam Daly <elam.daly@gmail.com>
2022-03-18 14:07:19 -0400

    route changes

--- build/index.js
+++ build/index.js
@@ -4,15 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
-const fastify = (0, fastify_1.default)({
+const fastify_static_1 = __importDefault(require("fastify-static"));
+const path_1 = __importDefault(require("path"));
+const server1 = (0, fastify_1.default)({
     logger: true,
 });
-fastify.get('/', (request, reply) => {
+server1.register(fastify_static_1.default, {
+    root: path_1.default.join(__dirname, '../html'),
+    prefix: '/public/'
 });
-fastify.get('/download', (request, reply) => {
-    console.log('downloading...');
+const server2 = (0, fastify_1.default)({
+    logger: true,
 });
-fastify.listen(3000, (err, address) => {
+server1.get('/download', (request, reply) => {
+    console.log('downloading...!!!');
+    reply;
+});
+server1.listen(3000, (err, address) => {
+    if (err)
+        throw err;
+});
+server2.get('/downloadFile', (request, reply) => {
+    console.log('getting file...');
+});
+server2.listen(3001, (err, address) => {
     if (err)
         throw err;
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import Fastify, { FastifyInstance } from 'fastify';
+import fastifyStatic from 'fastify-static';
+import path from 'path';
 
 const server1: FastifyInstance = Fastify({
     logger: true,
+});
+server1.register(fastifyStatic, {
+    root: path.join(__dirname, '../html'),
+    prefix: '/public/'
 });
 
 const server2: FastifyInstance = Fastify({
     logger: true,
 });
 
-server1.get('/', (request, reply) => {
-    
-});
-
 server1.get('/download', (request, reply) => {
-    console.log('downloading...');
+    console.log('downloading...!!!');
+    reply;
 });
 server1.listen(3000, (err, address) => {
     if(err) throw err;
@@ -21,6 +24,7 @@ server1.listen(3000, (err, address) => {
 
 server2.get('/downloadFile', (request, reply) => {  
     console.log('getting file...');
+
 });
 server2.listen(3001, (err, address) => {
     if(err) throw err;
